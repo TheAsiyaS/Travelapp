126c99e1dfd80c437bba6bdd4626a6f4728e4fd5
TheAsiyaS <135627861+TheAsiyaS@users.noreply.github.com>
2023-09-17 09:29:42 +0530

    Error handleing (failures)

---
+++ lib/Core/Failures/Failures.dart
@@ -0,0 +1,9 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'Failures.freezed.dart';
+
+@freezed
+class mainFailure with _$mainFailure {
+  const factory mainFailure.clientFailure() = _ClientFailure;
+  const factory mainFailure.serverFailure() = _ServerFailure;
+
+}
