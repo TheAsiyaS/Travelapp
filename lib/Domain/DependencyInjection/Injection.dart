811a5e6c9782d9c9600255d3bb3436ae02df105a
TheAsiyaS <135627861+TheAsiyaS@users.noreply.github.com>
2023-09-17 09:57:37 +0530

    Dependency injection setup . some modification in I_hotel_service

---
+++ lib/Domain/DependencyInjection/Injection.dart
@@ -0,0 +1,11 @@
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+import 'Injection.config.dart';
+
+
+final getIt = GetIt.instance;
+@InjectableInit()
+Future<void> configInjection() async {
+  await $initGetIt(getIt, environment: Environment.prod);
+}
---
+++ lib/Domain/HotelModel/I_HotelModel_Service.dart
@@ -0,0 +1,7 @@
+import 'package:travelapp/Core/Failures/Failures.dart';
+import 'package:travelapp/Domain/HotelModel/hotel_model/hotel_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class IhoteleRepo {
+  Future<Either<mainFailure, List<HotelModel>>> getHotelDetails();
+}
