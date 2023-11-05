30c23676d0885eec8cfe1a349966b0157a101fc4
TheAsiyaS <135627861+TheAsiyaS@users.noreply.github.com>
2023-11-05 22:21:22 +0530

    advanture like func & firebase add done

--- lib/presentation/Home/SubScreens/Advanture.dart
+++ lib/presentation/Home/SubScreens/Advanture.dart
@@ -1,9 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:travelapp/Application/Hotel&Place_Bloc/hotel_place_bloc.dart';
+import 'package:travelapp/Domain/DB/Infrastructure/FirestoreMethod.dart';
 import 'package:travelapp/common/Icons.dart';
 import 'package:travelapp/common/colours.dart';
+import 'package:travelapp/main.dart';
 import 'package:travelapp/widgets/CircularProgressIndicator.dart';
 import 'package:travelapp/widgets/ContainerWithWidget.dart';
 import 'package:travelapp/widgets/SearchItemDetailed.dart';
@@ -35,6 +38,8 @@ class Adavanture extends StatelessWidget {
       'Victoria Falls, Zambia/Zimbabwe',
       'Amazon Rainforest, Brazil/Peru',
     ];
+    final ValueNotifier<List<String>> likes = ValueNotifier([]);
+
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -74,12 +79,12 @@ class Adavanture extends StatelessWidget {
                           Navigator.of(context).push(MaterialPageRoute(
                               builder: (context) => SearchItemDetailed(
                                   imageurl: data.largeImageUrl!,
-                                  suburls: [],
+                                  suburls: const [],
                                   price: "${data.comments}0",
                                   title: places[index % places.length],
                                   subtitle:
                                       ' rope climbing exercises, obstacle courses, bouldering, rock climbing, target oriented activities, and zip-lines. They are usually intended for recreation.',
-                                  rating:  data.comments!,
+                                  rating: data.comments!,
                                   reviewNo: data.comments!.toString(),
                                   obj: 'obj')));
                         },
@@ -109,17 +114,133 @@ class Adavanture extends StatelessWidget {
                                       ),
                                     ),
                                     const Spacer(),
-                                    ConatinerwithWidget(
-                                        height: 50,
-                                        width: 50,
-                                        containerdecoration: BoxDecoration(
-                                            color: klightwhite,
-                                            borderRadius:
-                                                BorderRadius.circular(10)),
-                                        childwidget: const Icon(
-                                          kfavorite,
-                                          color: kDominantcolor,
-                                        )),
+                                    FutureBuilder(
+                                        future: FirebaseFirestore.instance
+                                            .collection("SavedPlaces")
+                                            .where('userid',
+                                                isEqualTo: currentuserdata.uid)
+                                            .get(),
+                                        builder: (context, snapshot) {
+                                          if (snapshot.connectionState ==
+                                              ConnectionState.waiting) {
+                                            return const CircularProgressIndicator(
+                                              strokeWidth: 2,
+                                              color: kwhite,
+                                            );
+                                          } else if (snapshot.hasError) {
+                                            return const CircularProgressIndicator(
+                                              strokeWidth: 2,
+                                              color: kRed,
+                                            );
+                                          } else {
+                                            if (currentuserdata.uid != null) {
+                                              // Extract only the numeric part of the PlaceId if it exists
+                                              List<String> likedPostNumericIds =
+                                                  snapshot.data!.docs
+                                                      .map((doc) {
+                                                // Check if the 'PlaceId' field exists in the document
+                                                if (doc
+                                                    .data()
+                                                    .containsKey('placeId')) {
+                                                  String fullId = doc
+                                                      .get('placeId')
+                                                      .toString();
+                                                  var parts = fullId.split('-');
+                                                  return parts.first;
+                                                } else {
+                                                  return '102840';
+                                                }
+                                              }).toList();
+
+                                              likes.value.clear();
+                                              likes.value = List.from(
+                                                  likes.value)
+                                                ..addAll(likedPostNumericIds);
+                                            }
+                                            print('likes : ${likes.value}');
+                                            return ValueListenableBuilder(
+                                                valueListenable: likes,
+                                                builder: (context, value, _) {
+                                                  return ConatinerwithWidget(
+                                                      height: 50,
+                                                      width: 50,
+                                                      containerdecoration:
+                                                          BoxDecoration(
+                                                              color:
+                                                                  klightwhite,
+                                                              borderRadius:
+                                                                  BorderRadius
+                                                                      .circular(
+                                                                          10)),
+                                                      childwidget: IconButton(
+                                                        onPressed: () async {
+                                                          print(
+                                                              'id : ${data.id}');
+                                                          if (!likes.value
+                                                              .contains(data.id
+                                                                  .toString())) {
+                                                            likes.value = List
+                                                                .from(
+                                                                    likes.value)
+                                                              ..add(data.id
+                                                                  .toString());
+
+                                                            await FirestoreMethods().placesaved(
+                                                                placeId:
+                                                                    "${data.id}-${currentuserdata.uid}",
+                                                                name: places[index %
+                                                                    places
+                                                                        .length],
+                                                                decription:
+                                                                    'Popular tourists place in the world . ${places[index]}\'s top place . good air condition & safe also.',
+                                                                rating: data
+                                                                    .comments!,
+                                                                imageurl: data
+                                                                    .largeImageUrl!,
+                                                                subimageurl: [],
+                                                                price:
+                                                                    '${data.imageHeight! / 5}',
+                                                                reviewno:
+                                                                    '${data.comments}',
+                                                                username:
+                                                                    currentuserdata
+                                                                        .username,
+                                                                userid:
+                                                                    currentuserdata
+                                                                        .uid!,
+                                                                userimageurl:
+                                                                    currentuserdata
+                                                                        .photoUrl);
+                                                          } else {
+                                                            print("delete");
+                                                            likes.value = List
+                                                                .from(
+                                                                    likes.value)
+                                                              ..remove(data.id
+                                                                  .toString());
+
+                                                            await FirestoreMethods()
+                                                                .deletePlaceSaved(
+                                                                    placedId:
+                                                                        "${data.id}-${currentuserdata.uid}");
+                                                          }
+                                                        },
+                                                        icon: likes.value
+                                                                .contains(data
+                                                                    .id
+                                                                    .toString())
+                                                            ? const Icon(
+                                                                kfavorite,
+                                                              )
+                                                            : const Icon(
+                                                                kfavoriteOutline,
+                                                                color:
+                                                                    kDominantcolor,
+                                                              ),
+                                                      ));
+                                                });
+                                          }
+                                        })
                                   ],
                                 ),
                               )
