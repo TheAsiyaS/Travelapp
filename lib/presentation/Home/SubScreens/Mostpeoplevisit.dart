c624b413d8b32debd853b18fb0462fff08d0c317
TheAsiyaS <135627861+TheAsiyaS@users.noreply.github.com>
2023-11-05 22:16:04 +0530

    mostPeopleVisit page like func & firebase add completed

--- lib/presentation/Home/SubScreens/Mostpeoplevisit.dart
+++ lib/presentation/Home/SubScreens/Mostpeoplevisit.dart
@@ -1,10 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:travelapp/Application/Hotel&Place_Bloc/hotel_place_bloc.dart';
 import 'package:travelapp/Application/HotelBloc/hotel_bloc.dart';
+import 'package:travelapp/Domain/DB/Infrastructure/FirestoreMethod.dart';
 import 'package:travelapp/common/Icons.dart';
 import 'package:travelapp/common/Sizedboxes.dart';
 import 'package:travelapp/common/colours.dart';
+import 'package:travelapp/main.dart';
 import 'package:travelapp/widgets/CircularProgressIndicator.dart';
 import 'package:travelapp/widgets/ContainerWithWidget.dart';
 import 'package:travelapp/widgets/IconButton.dart';
@@ -33,6 +36,8 @@ class Mostpeoplevisit extends StatelessWidget {
       'Compiègne',
       'Chantilly',
     ];
+    final ValueNotifier<List<String>> likes = ValueNotifier([]);
+
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -76,20 +81,131 @@ class Mostpeoplevisit extends StatelessWidget {
                                   padding: const EdgeInsets.all(8.0),
                                   child: Align(
                                     alignment: Alignment.centerRight,
-                                    child: ConatinerwithWidget(
-                                      height: 50,
-                                      width: 50,
-                                      containerdecoration: BoxDecoration(
-                                          color: klightwhite,
-                                          borderRadius:
-                                              BorderRadius.circular(15)),
-                                      childwidget: IconButtonWidget(
-                                          onPressFunc: () {},
-                                          iconwidget: const Icon(
-                                            kfavoriteOutline,
-                                            color: kDominantcolor,
-                                          )),
-                                    ),
+                                    child: FutureBuilder(
+                                        future: FirebaseFirestore.instance
+                                            .collection("SavedPlaces")
+                                            .where('userid',
+                                                isEqualTo: currentuserdata.uid)
+                                            .get(),
+                                        builder: (context, snapshot) {
+                                          if (snapshot.connectionState ==
+                                              ConnectionState.waiting) {
+                                            return const CircularProgressIndicator(
+                                              strokeWidth: 2,
+                                              color: kwhite,
+                                            );
+                                          } else if (snapshot.hasError) {
+                                            return const CircularProgressIndicator(
+                                              strokeWidth: 2,
+                                              color: kRed,
+                                            );
+                                          } else {
+                                            if (currentuserdata.uid != null) {
+                                              // Extract only the numeric part of the PlaceId if it exists
+                                              List<String> likedPostNumericIds =
+                                                  snapshot.data!.docs
+                                                      .map((doc) {
+                                                // Check if the 'PlaceId' field exists in the document
+                                                if (doc
+                                                    .data()
+                                                    .containsKey('placeId')) {
+                                                  String fullId = doc
+                                                      .get('placeId')
+                                                      .toString();
+                                                  var parts = fullId.split('-');
+                                                  return parts.first;
+                                                } else {
+                                                  return '102840';
+                                                }
+                                              }).toList();
+
+                                              likes.value.clear();
+                                              likes.value = List.from(
+                                                  likes.value)
+                                                ..addAll(likedPostNumericIds);
+                                            }
+                                            print('likes : ${likes.value}');
+                                            return ValueListenableBuilder(
+                                                valueListenable: likes,
+                                                builder: (context, value, _) {
+                                                  return ConatinerwithWidget(
+                                                    height: 50,
+                                                    width: 50,
+                                                    containerdecoration:
+                                                        BoxDecoration(
+                                                            color: klightwhite,
+                                                            borderRadius:
+                                                                BorderRadius
+                                                                    .circular(
+                                                                        15)),
+                                                    childwidget:
+                                                        IconButtonWidget(
+                                                      onPressFunc: () async {
+                                                        print(
+                                                            'id : ${data.id}');
+                                                        if (!likes.value
+                                                            .contains(data.id
+                                                                .toString())) {
+                                                          likes.value = List
+                                                              .from(likes.value)
+                                                            ..add(data.id
+                                                                .toString());
+
+                                                          await FirestoreMethods().placesaved(
+                                                              placeId:
+                                                                  "${data.id}-${currentuserdata.uid}",
+                                                              name: places[index %
+                                                                  places
+                                                                      .length],
+                                                              decription:
+                                                                  'Popular tourists place in the world . ${places[index]}\'s top place . good air condition & safe also.',
+                                                              rating: data
+                                                                  .comments!,
+                                                              imageurl: data
+                                                                  .largeImageUrl!,
+                                                              subimageurl: [],
+                                                              price:
+                                                                  '${data.imageHeight! / 5}',
+                                                              reviewno:
+                                                                  '${data.comments}',
+                                                              username:
+                                                                  currentuserdata
+                                                                      .username,
+                                                              userid:
+                                                                  currentuserdata
+                                                                      .uid!,
+                                                              userimageurl:
+                                                                  currentuserdata
+                                                                      .photoUrl);
+                                                        } else {
+                                                          print("delete");
+                                                          likes.value = List
+                                                              .from(likes.value)
+                                                            ..remove(data.id
+                                                                .toString());
+
+                                                          await FirestoreMethods()
+                                                              .deletePlaceSaved(
+                                                                  placedId:
+                                                                      "${data.id}-${currentuserdata.uid}");
+                                                        }
+                                                      },
+                                                      iconwidget: likes.value
+                                                              .contains(data.id
+                                                                  .toString())
+                                                          ? const Icon(
+                                                              kfavorite,
+                                                            )
+                                                          : const Icon(
+                                                              kfavoriteOutline,
+                                                              color:
+                                                                  kDominantcolor,
+                                                            ),
+                                                    ),
+                                                  );
+                                                });
+                                          }
+                                        }),
                                   ),
                                 ),
                                 const Spacer(),
