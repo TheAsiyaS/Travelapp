3bac6a97a65cba8cfcc9236ebcc9e8d79e161ef1
TheAsiyaS <135627861+TheAsiyaS@users.noreply.github.com>
2023-11-08 07:45:56 +0530

    firebase data shown in ui (facourite paces) done

--- lib/presentation/Home/DrawerScreens/FavouritePlace.dart
+++ lib/presentation/Home/DrawerScreens/FavouritePlace.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:travelapp/common/Icons.dart';
 import 'package:travelapp/common/Sizedboxes.dart';
 import 'package:travelapp/common/Styles.dart';
 import 'package:travelapp/common/colours.dart';
+import 'package:travelapp/main.dart';
 import 'package:travelapp/widgets/AdvitismentCard.dart';
 import 'package:travelapp/widgets/ContainerWithWidget.dart';
 
@@ -29,79 +31,108 @@ class FavouritePlaces extends StatelessWidget {
               SizedBox(
                 height: size.height / 1.1,
                 width: size.width,
-                child: ListView.separated(
-                    itemBuilder: (context, index) {
-                      return Card(
-                        elevation: 10,
-                        child: SizedBox(
-                          height: size.height / 4,
-                          width: size.width / 2,
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  const Text(
-                                    'Place Name',
-                                    style: textstyle,
-                                  ),
-                                  w20,
-                                  ConatinerwithWidget(
-                                      containerdecoration: BoxDecoration(
-                                          color: kDominantTransdark,
-                                          borderRadius:
-                                              BorderRadiusDirectional.circular(
-                                                  15)),
-                                      childwidget: IconButton(
-                                          onPressed: () {},
-                                          icon: const Icon(
-                                            kfavorite,
-                                            color: kdominatgrey,
-                                          )),
-                                      height: 50,
-                                      width: 50)
-                                ],
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  ConatinerwithWidget(
-                                      containerdecoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(20),
-                                          image: const DecorationImage(
-                                              image: NetworkImage(
-                                                  'https://geographical.co.uk/wp-content/uploads/Photographing-mountains-in-spring-1200x800.jpg'),
-                                              fit: BoxFit.cover)),
-                                      childwidget: h10,
-                                      height: size.height / 6,
-                                      width: size.width / 2),
-                                  const Column(
+                child: FutureBuilder(
+                    future: FirebaseFirestore.instance
+                        .collection("SavedPlaces")
+                        .where('userid', isEqualTo: currentuserdata.uid)
+                        .get(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator(
+                          strokeWidth: 2,
+                          color: kwhite,
+                        );
+                      } else if (snapshot.hasError) {
+                        return const Center(
+                          child: Text('Some error occured!!'),
+                        );
+                      } else if (snapshot.data!.docs.isEmpty) {
+                        return const Center(
+                          child: Text('Currently you have no Favourite places'),
+                        );
+                      } else {
+                        return ListView.separated(
+                            itemBuilder: (context, index) {
+                              final data = snapshot.data!.docs[index];
+                              return Card(
+                                elevation: 10,
+                                child: SizedBox(
+                                  height: size.height / 4,
+                                  width: size.width / 2,
+                                  child: Column(
                                     children: [
-                                      Text(
-                                        '\$934',
-                                        style: textstyle,
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceAround,
+                                        children: [
+                                          Text(
+                                            data['name'],
+                                            style: textstyle,
+                                          ),
+                                          w20,
+                                          ConatinerwithWidget(
+                                              containerdecoration: BoxDecoration(
+                                                  color: kDominantTransdark,
+                                                  borderRadius:
+                                                      BorderRadiusDirectional
+                                                          .circular(15)),
+                                              childwidget: IconButton(
+                                                  onPressed: () {},
+                                                  icon: const Icon(
+                                                    kfavorite,
+                                                    color: kdominatgrey,
+                                                  )),
+                                              height: 50,
+                                              width: 50)
+                                        ],
                                       ),
-                                      SizedBox(
-                                          height: 50,
-                                          width: 150,
-                                          child: Text(
-                                              'Descriptin about the place want to visit '))
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceAround,
+                                        children: [
+                                          ConatinerwithWidget(
+                                              containerdecoration:
+                                                  BoxDecoration(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              20),
+                                                      image: DecorationImage(
+                                                          image: NetworkImage(
+                                                              data['imageurl']),
+                                                          fit: BoxFit.cover)),
+                                              childwidget: h10,
+                                              height: size.height / 6,
+                                              width: size.width / 2),
+                                          Column(
+                                            children: [
+                                              Text(
+                                                "\$${data['price']}",
+                                                style: textstyle,
+                                              ),
+                                              SizedBox(
+                                                  height: 50,
+                                                  width: 150,
+                                                  child: Text(
+                                                    data['decription'],
+                                                    maxLines: 3,
+                                                    overflow:
+                                                        TextOverflow.ellipsis,
+                                                  ))
+                                            ],
+                                          )
+                                        ],
+                                      )
                                     ],
-                                  )
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                    separatorBuilder: (context, index) {
-                      return const Divider();
-                    },
-                    itemCount: 10),
+                                  ),
+                                ),
+                              );
+                            },
+                            separatorBuilder: (context, index) {
+                              return const Divider();
+                            },
+                            itemCount: snapshot.data!.docs.length);
+                      }
+                    }),
               )
             ],
           ),
@@ -129,5 +160,5 @@ class FavouritePlaces extends StatelessWidget {
         ],
       )),
     );
-  }
+  } //https://youtu.be/OMKDBXi8fcI?si=S0tUxbnDDNglA1wQ
 }
