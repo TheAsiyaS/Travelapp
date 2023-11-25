9f775ad62e6eddb64632c8a23184a11283628b59
TheAsiyaS <135627861+TheAsiyaS@users.noreply.github.com>
2023-11-26 04:26:19 +0530

    search place screen value added

--- lib/widgets/onssearchtimescreen.dart
+++ lib/widgets/onssearchtimescreen.dart
@@ -16,8 +16,20 @@ class OnsearchtimeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-        final placeController = TextEditingController();
-
+    final placeController = TextEditingController();
+    ValueNotifier<String> searchvalue =
+        ValueNotifier('Search your hapiness......');
+    final searchplacesabout = [
+      'on the coast of the Persian Gulf',
+      'in the continent of Asia',
+      'located in South Asia'
+    ];
+    final searchplacesname = ['Dubai', 'India', 'maldives'];
+    final searchplacesimage = [
+      'https://lp-cms-production.imgix.net/features/2017/09/dubai-marina-skyline-2c8f1708f2a1.jpg',
+      'https://www.g20.org/content/dam/gtwenty/gtwenty_new/explore_india/Culture%20Heritage%20of%20India_1.jpg',
+      'https://media2.thrillophilia.com/images/photos/000/082/497/original/1675261154_shutterstock_2148766633.jpg?width=975&height=600'
+    ];
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -41,24 +53,33 @@ class OnsearchtimeScreen extends StatelessWidget {
                 ),
                 ConatinerwithWidget(
                     containerdecoration: const BoxDecoration(),
-                    childwidget: CupertinotextfieldWidget(
-                      controller: placeController,
-                        onchanged: (value) {},
-                        onsubmitted: (value) {  
-                          log(value);
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => SearchResult(querry: value,)));
-                        },
-                        placeholderText: 'Search your hapiness......',
-                        placeholderStyle: const TextStyle(color: kdominatgrey),
-                        boxDecoration: BoxDecoration(
-                            color: kwhite,
-                            borderRadius: BorderRadius.circular(15)),
-                        prefixWidget: w10,
-                        suffixWidget: h10,
-                        keybodtype: TextInputType.name,
-                        obscureText: false,
-                        style: const TextStyle()),
+                    childwidget: ValueListenableBuilder(
+                      valueListenable: searchvalue,
+                      builder: (context, value,_) {
+                        return CupertinotextfieldWidget(
+                            controller: placeController,
+                            onchanged: (value) {},
+                            onsubmitted: (value) {
+                               searchvalue.value =value;
+                          searchvalue.notifyListeners();
+                              log(value);
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => SearchResult(
+                                        querry: searchvalue.value,
+                                      )));
+                            },
+                            placeholderText: searchvalue.value,
+                            placeholderStyle: const TextStyle(color: kdominatgrey),
+                            boxDecoration: BoxDecoration(
+                                color: kwhite,
+                                borderRadius: BorderRadius.circular(15)),
+                            prefixWidget: w10,
+                            suffixWidget: h10,
+                            keybodtype: TextInputType.name,
+                            obscureText: false,
+                            style: const TextStyle());
+                      }
+                    ),
                     height: 50,
                     width: size.width / 1.2)
               ],
@@ -72,19 +93,23 @@ class OnsearchtimeScreen extends StatelessWidget {
                 childwidget: ListView.separated(
                     itemBuilder: (context, index) {
                       return ListTile(
+                        onTap: () {
+                          placeController.text = searchplacesname[index];
+                          searchvalue.value = searchplacesname[index];
+                          searchvalue.notifyListeners();
+                        },
                         leading: ConatinerwithWidget(
                             containerdecoration: BoxDecoration(
-                                image: const DecorationImage(
-                                  image: NetworkImage(
-                                      'https://www.studentuniverse.com/blog/wp-content/uploads/2014/04/Santorini-Greece.jpg'),
+                                image: DecorationImage(
+                                  image: NetworkImage(searchplacesimage[index]),
                                   fit: BoxFit.cover,
                                 ),
                                 borderRadius: BorderRadius.circular(10)),
                             childwidget: h10,
                             height: 50,
                             width: 50),
-                        title: const Text('Search Place'),
-                        subtitle: const Text('About that place'),
+                        title: Text(searchplacesname[index]),
+                        subtitle: Text(searchplacesabout[index]),
                         trailing: IconButtonWidget(
                             onPressFunc: () {},
                             iconwidget: const Icon(
@@ -96,7 +121,7 @@ class OnsearchtimeScreen extends StatelessWidget {
                     separatorBuilder: (context, index) {
                       return const Divider();
                     },
-                    itemCount: 10),
+                    itemCount: 3),
                 height: size.height / 1.5,
                 width: size.width)
           ],
